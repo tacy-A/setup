448f9fcd2ed5c1b2013d8bcdd35e734e41fdb51a
tacy <34135823+tacy-A@users.noreply.github.com>
2023-12-05 18:19:04 +0900

    add clt install process

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+echo "Installing Xcode Command Line Tools..."
+xcode-select --install
+install_status=$?
+
+if [ $install_status -ne 0 ]; then
+  echo "Error: Failed to install Xcode Command Line Tools."
+  exit 1
+fi
+
+echo "Xcode Command Line Tools has been successfully installed."
+echo "Install the setup repository."
+
 read -p "Enter your Gihub username: " username
 read -sp "Enter your Gihub Access token: " token
 echo
